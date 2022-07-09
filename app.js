cee041b091eae4e57fc6f8b95058bf25f8cdf5bd
Your Name <you@example.com>
2022-07-09 11:27:33 -0600

    completed up to step 8

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let counter = 0
-while(counter <50){
+while(counter <51){
     console.log(counter);
     counter = counter + 1;
 }
 
-while(counter > -50){
+while(counter > -51){
     console.log(counter);
     counter = counter - 1;
 }
 
-while(counter <100){
+while(counter <101){
     console.log(counter);
     counter = counter + 2;
+}
+
+// Another Knowledge check
+
+let names =[`josh`, `james`, `marc`];
+let number = 0;
+let score = [4, 4, 3];
+let injured = [true, false, true]
+
+while(number < names.length){
+console.log(names[number]);
+console.log(score[number])
+number = number + 1;
 }
