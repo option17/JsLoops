002217b8c8db0aa4458629902711af29b784564c
Your Name <you@example.com>
2022-07-09 10:06:06 -0600

    Finished KC

---
+++ app.js
@@ -0,0 +1,15 @@
+let counter = 0
+while(counter <50){
+    console.log(counter);
+    counter = counter + 1;
+}
+
+while(counter > -50){
+    console.log(counter);
+    counter = counter - 1;
+}
+
+while(counter <100){
+    console.log(counter);
+    counter = counter + 2;
+}
